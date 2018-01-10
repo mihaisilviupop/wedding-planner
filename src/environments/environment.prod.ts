63f5410d218fa81f1f0c8a8388a7631c648df57a
mihaisilviupop <mihai.silviu.pop@gmail.com>
2018-01-11 01:19:37 +0200

    added bootstrap, firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDIQVmCYNuhqSYn4X4Yaj-mtWglioY8jR4",
+    authDomain: "plan-nunta.firebaseapp.com",
+    databaseURL: "https://plan-nunta.firebaseio.com",
+    projectId: "plan-nunta",
+    storageBucket: "plan-nunta.appspot.com",
+    messagingSenderId: "776846804690"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDIQVmCYNuhqSYn4X4Yaj-mtWglioY8jR4",
+    authDomain: "plan-nunta.firebaseapp.com",
+    databaseURL: "https://plan-nunta.firebaseio.com",
+    projectId: "plan-nunta",
+    storageBucket: "plan-nunta.appspot.com",
+    messagingSenderId: "776846804690"
+  }
 };
