b03754ffe0fe463685cc20ab88fcf27c26083376
mihaisilviupop <mihai.silviu.pop@gmail.com>
2019-08-07 00:35:28 +0300

    some old fucking work i dont want to lose

--- src/app/models/nuntas.model.ts
+++ src/app/models/nuntas.model.ts
@@ -1,3 +1,5 @@
+import { FormControl, Validators, } from "@angular/forms";
+
 export class Nuntas {
   $key: string;
   nume: string;
@@ -7,4 +9,84 @@ export class Nuntas {
   parte: string;
   nrInsotitori?: number;
   categorii: string;
+
+  constructor(pers?: Nuntas) {
+    if (pers) {
+      this.$key = pers.$key;
+      this.nume = pers.nume;
+      this.prenume = pers.prenume;
+      this.invitat = pers.invitat;
+      this.confirmat = pers.confirmat;
+      this.parte = pers.parte;
+      this.nrInsotitori = pers.nrInsotitori;
+      this.categorii = pers.categorii;
+    }
+  }
 }
+
+export class NuntasForm {
+  $key;
+  nume = <FormProp>{
+    value: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(20)])
+  };
+  prenume = <FormProp>{
+    value: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(20)])
+  };
+  invitat?: boolean;
+  confirmat?: boolean;
+  parte = <FormProp>{
+    value: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(20)])
+  };
+  nrInsotitori?: number;
+  categorii: string;
+
+  constructor(pers?: Nuntas) {
+    if (pers) {
+      this.$key = pers.$key;
+      this.nume.value.setValue(pers.nume);
+      this.prenume.value.setValue(pers.prenume);
+      this.invitat = pers.invitat;
+      this.confirmat = pers.confirmat;
+      this.parte.value.setValue(pers.parte);
+      this.nrInsotitori = pers.nrInsotitori;
+      this.categorii = pers.categorii;
+
+      this.nume.error = () => {
+        return this.getError('nume');
+      }
+      this.prenume.error = () => {
+        return this.getError('prenume');
+      }
+      this.parte.error = () => {
+        return this.getError('parte');
+      }
+    }
+  }
+
+  private getError(field: string): string {
+    return this[field].value.hasError('required') ? 'Camp obligatoriu' :
+      this[field].value.hasError('minLength') ? 'Prea putine caractere' :
+        this[field].value.hasError('maxLength') ? 'Prea multe caractere' : '';
+  }
+
+  getNuntas(): Nuntas {
+    return new Nuntas({
+      $key: this.$key,
+      categorii: this.categorii,
+      confirmat: this.confirmat,
+      invitat: this.invitat,
+      nrInsotitori: this.nrInsotitori,
+      nume: this.nume.value.value,
+      prenume: this.prenume.value.value,
+      parte: this.parte.value.value
+    });
+  }
+
+}
+
+interface FormProp {
+  value: FormControl;
+  error(): string;
+}
+
+
--- src/app/nuntas-nou/nuntas-nou.component.ts
+++ src/app/nuntas-nou/nuntas-nou.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { Nuntas } from '../models/nuntas.model';
-import { FormBuilder, FormGroup, Validator } from "@angular/forms";
+import { Nuntas, NuntasForm } from '../models/nuntas.model';
+import { FormBuilder, FormGroup, FormControl, Validators } from "@angular/forms";
 import { NuntasiService } from "../services/nuntasi.service";
 
 @Component({
@@ -10,8 +10,10 @@ import { NuntasiService } from "../services/nuntasi.service";
   styleUrls: ['./nuntas-nou.component.css']
 })
 export class NuntasNouComponent {
-  nuntas: Nuntas = new Nuntas();
+  nuntas = new NuntasForm();
   options: FormGroup;
+
+
 
   constructor(
     public dialogRef: MatDialogRef<NuntasNouComponent>,
@@ -20,7 +22,7 @@ export class NuntasNouComponent {
     private nuntasiServie: NuntasiService) {
     if (data && data.key) {
       this.nuntasiServie.getNuntas(data.key).subscribe(item => {
-        this.nuntas = item;
+        this.nuntas = new NuntasForm(item);
       });
     }
     this.options = fb.group({
@@ -31,7 +33,7 @@ export class NuntasNouComponent {
 
   confirm() {
     this.dialogRef.close({
-      nuntas: this.nuntas,
+      nuntas: this.nuntas.getNuntas(),
       key: this.data && this.data.key ? this.data.key : undefined
     });
   }
